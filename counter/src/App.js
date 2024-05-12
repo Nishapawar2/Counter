bd65c320c53b7a8044d2d41e2446e114ed737497
unknown <nisha.pawar2231@gmail.com>
2024-05-12 15:47:26 +0530

    first commit

---
+++ counter/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+  increment = () => {
+    this.setState((prev) => ({ count: prev.count + 1 }));
+  };
+
+  decrement = () => {
+    this.setState((prev) => ({ count: prev.count - 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default App;
